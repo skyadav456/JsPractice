0988f892cbaf9f181ce81cf904edcc4e2bb77b6c
skyadav456 <87852635+skyadav456@users.noreply.github.com>
2023-12-21 23:32:14 +0000

    Change done

---
+++ canditional/canditional.js
@@ -0,0 +1,48 @@
+//console.log("Canditional Statement")
+
+/* Determine wheather the piece of code can run or not 
+
+  there are multiple type of canditional statement
+   1. if -else   - check the candition true or false if true it execute otherwise not execute 
+   2. nested if - else
+   3. switch
+*/
+
+/* 
+syntax --  if statemet
+
+        if(candition){
+            // code
+        }
+
+
+syntax-  if- else
+    
+    if(statement){
+        // piece of code
+    }
+    else{
+        // piece of code
+    }
+*/
+
+// example
+
+let name = "sharad"
+
+if(name=="sharad"){
+    console.log(`welcome ${name} to my blog`)
+}
+ 
+
+console.log("--------------------------------------------")
+
+let a = 10
+let b=34
+
+if(a>b){
+    console.log("a is greater than b")
+}
+else{
+    console.log("a is less than b ")
+}
