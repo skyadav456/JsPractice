37a9ff9d552e9b789093ab038aba02c836b6ec0c
skyadav456 <87852635+skyadav456@users.noreply.github.com>
2023-12-21 23:46:07 +0000

    change done

---
+++ canditional/example1.js
@@ -0,0 +1,15 @@
+// create traffic light system that shows what to do  based on color
+
+let color ='red';
+
+if(color==='red'){
+    console.log("Please Stop!")
+}
+
+if(color==='green'){
+    console.log("Please Go!")
+}
+
+if(color==='yellow'){
+    console.log("Please slow down!")
+}
