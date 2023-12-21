872774d816c51b272f738a24469a01ccbd60da1e
skyadav456 <87852635+skyadav456@users.noreply.github.com>
2023-12-21 22:36:42 +0000

    Changes done

--- operator.js
+++ operator.js
@@ -1,1 +1,31 @@
 console.log("sharad")
+
+let a=2;
+let b= 4;
+//  arithmetic operator
+// console.log(a+b);
+// console.log(a*b);
+// console.log(a-b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+// unary operator
+
+// console.log(a++)  //2
+// console.log(++a)  //4
+// console.log(b++)  //4
+// console.log(++b)  //6
+
+
+// Assignment operator (=,+=,-=,*=,/=)
+
+let x=4;
+let y=5;
+
+console.log(y=x) // 4
+console.log(x+=y) //8
+console.log(x-=y)  //4
+console.log(x*=y) //16
+console.log(x/=y)  //4
+
