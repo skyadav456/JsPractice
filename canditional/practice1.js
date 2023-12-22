5245b29883207eda9282a2c201072c7f5b14d229
skyadav456 <87852635+skyadav456@users.noreply.github.com>
2023-12-22 00:04:09 +0000

    commit

---
+++ canditional/practice1.js
@@ -0,0 +1,22 @@
+// calculate pizza price based on size
+/*
+  if size is XL -- 300 Rs
+  if size L -- 250 rs
+  if Size M -- 200
+  if size small --100
+*/
+
+let size="XL";
+
+if(size==="XL"){
+    console.log("Price is Rs 300")
+}
+else if(size="L"){
+    console.log("THe price is Rs 250")
+}
+else if(size="M"){
+    console.log("THe price is Rs 200")
+}
+else {
+    console.log("THe price is Ts 100")
+}
